cccd1a9a8f19da19abb613ef231d7cb99c98937c
danibr1 <75913820+danibr1@users.noreply.github.com>
2021-03-23 00:55:07 +0100

    completed

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
 
 //  Elenco di email inserite nel database
  var email = ['prova@prova.it', 'email@email.it', 'test@test.it'];
- console.log(email);
+//  console.log(email);
  
 //  Richiede l'inserimento dell'email da controllare nel db
  var user = prompt( 'Inserisci l\'email ').toLowerCase();
@@ -33,18 +33,30 @@
  }
 
 
- /**
-  * GIOCO DEI DADI
-  * 1. Generare un numero random da 1 a 6 sia per il giocatore;
-  * 2. Generare un numero random da 1 a 6 sia per il PC; 
-  * 2. Confronta i numeri random tra giocatore e pc;
-  * 3. Stabilisce il vincitore in base a chi fa il punteggio più alto, oppure stabilisci il pareggio * se i numeri sono uguali
-  */
+/** 
+ * GIOCO DEI DADI
+ * 1. Generare un numero random da 1 a 6 sia per il giocatore;
+ * 2. Generare un numero random da 1 a 6 sia per il PC; 
+ * 2. Confronta i numeri random tra giocatore e pc;
+ * 3. Stabilisce il vincitore in base a chi fa il punteggio più alto, oppure stabilisci il pareggio * se i numeri sono uguali
+ */
+ alert('Giochiamo ai dadi!');
+console.log('GIOCHIAMO AI DADI!')
 
-//  Lancio dado user
+ var user = prompt( 'Inserisci il tuo nome:')
+
+ //  Lancio dado user
  var dadoUser = Math.floor(Math.random() * 6) + 1;
- console.log('Dado del giocatore: è uscito il numero: ' + dadoUser);
+ console.log( user + ' lancia il dado ... è uscito il numero: ' + dadoUser);
 
-//  Lancio dado pc
+ //  Lancio dado pc
  var dadoPc = Math.floor(Math.random() * 6) + 1;
- console.log('Dado del PC: è uscito il numero: ' + dadoPc);
+ console.log('Il PC lancia il dado ... è uscito il numero: ' + dadoPc);
+
+ if (dadoUser > dadoPc){
+    console.log('Ha vinto ' + user);
+}else if (dadoUser < dadoPc){
+    console.log('Spiacente! Ha vinto il PC');
+}else{
+    console.log('Parità');
+}
